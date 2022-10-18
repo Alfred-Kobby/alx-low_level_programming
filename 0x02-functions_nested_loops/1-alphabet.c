9c0d15ddb0f19a332542d6f9ed718cb9c0d5a2a7
Alfred-Kobby <alfredternor@gmail.com>
2022-10-18 08:12:14 -0700

    suffer isomia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
  */
 void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		_putchar(word[i]);
+		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
