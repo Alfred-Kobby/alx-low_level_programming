ea87a2b20d196f672bd3014f8165f3e6b725761f
Alfred-Kobby <alfredternor@gmail.com>
2022-10-20 05:58:29 -0700

    multiplies

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - check description
+ * Description: returns a multiple of a nad b
+ * @a: first input parameter for mul
+ * @b: second input parameter for mul
+ * Return: return a*b
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,5 @@
 0-supper.c file: checks for uppercase character.
+
 1-isdigit.c file: checks if its digit
+
+2-mul.c file: multiplies a*b
