eaf53ba946ecd64ed92b048b6977d1a8317b895b
Alfred-Kobby <alfredternor@gmail.com>
2022-10-24 05:54:12 -0700

    98 battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - see description
+ * Description: accepts a pointer and updates it's values
+ * @n: input parameter for reset_to_98 function
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
