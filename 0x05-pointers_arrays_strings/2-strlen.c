a04ea7a870f21abe3e7c2ed59beee2c8b1b58c0a
Alfred-Kobby <alfredternor@gmail.com>
2022-10-24 06:36:19 -0700

    string lenght

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 /**
  * _strlen - see description
  * Description: returns the lenght of a string
- * @n: input parameter for _strlen function
+ * @s: input parameter for _strlen function
  * Return: lenght of string
  */
-int _strlen(char *n)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	while (*(n + 1) != '\0')
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
