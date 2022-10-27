ead149fd841b4c5f69b0373f60c6574e3f14a12c
Alfred-Kobby <alfredternor@gmail.com>
2022-10-27 04:02:11 -0700

    strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,20 +5,25 @@
  * @dest: input parameter for _strcat function
  * @src: input parameter for _strncat
  * @n: input paramter for _strncat
- * Return: nothing
+ * Return: pointer to result
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *out = dest;
-	int i = 0;
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
 
-	while (*dest)
-		*dest++;
-	while (i < n)
+	while (*src)
 	{
-		*dest++ = *src++;
-		i++;
+		srclen++;
+		src++;
 	}
+	while (*dest)
+		dest++;
+	if (n > srclen)
+		n = srclen;
+	src = start;
+	for (; i < n; i++)
+		*dest++ = *src++;
 	*dest = '\0';
-	return (out);
+	return (temp);
 }
