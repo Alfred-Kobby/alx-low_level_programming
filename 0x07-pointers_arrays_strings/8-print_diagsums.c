59c6b7eb81807e32a8cd4c934875216f621d5e4d
Alfred-Kobby <alfredternor@gmail.com>
2022-10-31 17:05:44 -0700

    print diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_diagsums - see description
  * Description: prints the sum of diaggonal
@@ -9,6 +10,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0, max = size * size, sum1 = 0, sum2 = 0;
+
 	for (; i < max; i += size + 1)
 		sum1 += a[i];
 	for (i = size - 1; i < max - 1; i += size - 1)
