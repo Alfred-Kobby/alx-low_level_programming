ede167f39e65f900ed3b78274754656888c26069
Alfred-Kobby <alfredternor@gmail.com>
2022-11-04 08:36:09 -0700

    4 add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - see description
+ * Description: entry to main application
+ * @argc: input parameter for main
+ * @argv: input vector for main
+ * Return: 1 if error or multiple of two numbers
+ */
+int main(int argc, char **argv)
+{
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+}
