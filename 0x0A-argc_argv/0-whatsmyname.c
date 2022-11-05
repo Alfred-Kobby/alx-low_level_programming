5ef1ee96257ce34b3c7378ee71c10227be051521
Alfred-Kobby <alfredternor@gmail.com>
2022-11-05 09:02:55 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - see description
  * Description: entry to main application
- * @agrc: input parameter for main
+ * @argc: input parameter for main
  * @argv: input vector for main
  * Return: 0 or 1
  */
-int main(int agrc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
