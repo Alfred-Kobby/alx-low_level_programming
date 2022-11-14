6064721f31c52f2ce306001f6b2e1fbfba8009b5
Alfred-Kobby <alfredternor@gmail.com>
2022-11-14 07:38:34 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - see decription
+ * Description: a struct for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: who is the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H */
