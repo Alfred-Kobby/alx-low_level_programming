682db98d7840312b0a2f381f6ea2bb37a8ad7e7a
Alfred-Kobby <alfredternor@gmail.com>
2022-11-14 05:57:19 -0800

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - see description
+ * Description: prints the name of the file it was compiled from
+ * Return: 0
+ */
+int main(void)
+{
+	printf(__FILE__);
+	return (0);
+}
