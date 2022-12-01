05da70c61e7f6e5ec696dab72156d62197d13138
Alfred-Kobby <alfredternor@gmail.com>
2022-12-01 13:00:47 -0800

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,27 +2,12 @@
 /**
  * print_binary - see description
  * Description:prints binary representation
- * @b: input parameter for print_binary
+ * @n: input parameter for print_binary
  * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
